e20401488544fbf842fc2e5c651e74a384418502
Valen Maranzana <valenmaranzanaa@gmail.com>
2022-10-24 06:24:19 +0000

    parte2

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -1,4 +1,22 @@
 class Attack < ApplicationRecord
     belongs_to :monster
     belongs_to :victim
+    validate :mayores
+
+    def mayores
+        m = Monster.find(monster_id)
+        v = Victim.find(victim_id)
+
+        now = Date.today
+        year = now.year - v.birthdate.year
+
+        if(now.month > v.birthdate.month)
+            year = year - 1
+        end   
+
+        if (m.scare_level > 5 && year < 18)
+              errors.add(:monster.scare_level, message:"Este monstruo no puede asustar victimas menores")          
+        end
+    end
+
 end
--- app/models/victim.rb
+++ app/models/victim.rb
@@ -1,4 +1,5 @@
 class Victim < ApplicationRecord
     validates :name, presence: true
     has_one :monster
+    has_one :attack, dependent: :destroy
 end
